e5ef605f8ff862cfe92caef9e8e1c1929c500fbf
Dirk Arnez <smalldirkalex@gmail.com>
2018-03-22 10:17:03 +0800

    - add gin

---
+++ lib/builder.go
@@ -0,0 +1,78 @@
+package gin
+
+import (
+	"fmt"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+type Builder interface {
+	Build() error
+	Binary() string
+	Errors() string
+}
+
+type builder struct {
+	dir       string
+	binary    string
+	errors    string
+	useGodep  bool
+	wd        string
+	buildArgs []string
+}
+
+func NewBuilder(dir string, bin string, useGodep bool, wd string, buildArgs []string) Builder {
+	if len(bin) == 0 {
+		bin = "bin"
+	}
+
+	// does not work on Windows without the ".exe" extension
+	if runtime.GOOS == "windows" {
+		if !strings.HasSuffix(bin, ".exe") { // check if it already has the .exe extension
+			bin += ".exe"
+		}
+	}
+
+	return &builder{dir: dir, binary: bin, useGodep: useGodep, wd: wd, buildArgs: buildArgs}
+}
+
+func (b *builder) Binary() string {
+	return b.binary
+}
+
+func (b *builder) Errors() string {
+	return b.errors
+}
+
+func (b *builder) Build() error {
+	args := append([]string{"go", "build", "-o", filepath.Join(b.wd, b.binary)}, b.buildArgs...)
+
+	var command *exec.Cmd
+	if b.useGodep {
+		args = append([]string{"godep"}, args...)
+	} else {
+		getCommand := exec.Command("go", "get", "./")
+		getCommand.Dir = b.dir
+		getCommand.Output()
+	}
+
+	command = exec.Command(args[0], args[1:]...)
+
+	command.Dir = b.dir
+
+	output, err := command.CombinedOutput()
+
+	if command.ProcessState.Success() {
+		b.errors = ""
+	} else {
+		b.errors = string(output)
+	}
+
+	if len(b.errors) > 0 {
+		return fmt.Errorf(b.errors)
+	}
+
+	return err
+}
