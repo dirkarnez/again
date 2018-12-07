91a678260b6a82e4ce2e8b3900d8e75bc1122aee
dirkarnez <smalldirkalex@gmail.com>
2018-12-07 17:15:16 +0800

    - add dependency-checking for necessary rebuild

--- lib/builder.go
+++ lib/builder.go
@@ -6,12 +6,15 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
+	"go/parser"
+	"go/token"
 )
 
 type Builder interface {
 	Build() error
 	Binary() string
 	Errors() string
+	ShouldDepEnsure() bool
 }
 
 type builder struct {
@@ -20,6 +23,7 @@ type builder struct {
 	errors    string
 	wd        string
 	buildArgs []string
+	importSet map[string]bool
 }
 
 func NewBuilder(dir string, bin string, wd string, buildArgs []string) Builder {
@@ -34,7 +38,7 @@ func NewBuilder(dir string, bin string, wd string, buildArgs []string) Builder {
 		}
 	}
 
-	return &builder{dir: dir, binary: bin, wd: wd, buildArgs: buildArgs}
+	return &builder{dir: dir, binary: bin, wd: wd, buildArgs: buildArgs, importSet: make(map[string]bool)}
 }
 
 func (b *builder) Binary() string {
@@ -48,18 +52,20 @@ func (b *builder) Errors() string {
 func (b *builder) Build() error {
 	args := append([]string{"go", "build", "-o", filepath.Join(b.wd, b.binary)}, b.buildArgs...)
 
-	depCommand := exec.Command("dep", "ensure")
-	depCommand.Dir = b.dir
-	out, _ := depCommand.CombinedOutput()
+	if b.ShouldDepEnsure() {
+		depCommand := exec.Command("dep", "ensure")
+		depCommand.Dir = b.dir
+		out, _ := depCommand.CombinedOutput()
+	
+		if depCommand.ProcessState.Success() {
+			b.errors = ""
+		} else {
+			b.errors = string(out)
+		}
 
-	if depCommand.ProcessState.Success() {
-		b.errors = ""
-	} else {
-		b.errors = string(out)
-	}
-
-	if len(b.errors) > 0 {
-		return fmt.Errorf(b.errors)
+		if len(b.errors) > 0 {
+			return fmt.Errorf(b.errors)
+		}
 	}
 
 	command := exec.Command(args[0], args[1:]...)
@@ -79,4 +85,24 @@ func (b *builder) Build() error {
 	}
 
 	return err
+}
+
+func (b *builder) ShouldDepEnsure () bool {
+	fileset := token.NewFileSet()
+
+	pkgs, _ := parser.ParseDir(fileset, b.wd, nil, parser.ImportsOnly)
+
+	var hasNewDependency bool = false
+	for _, pkg := range pkgs {
+		for _, astFile := range pkg.Files {
+			for _, importSpec := range astFile.Imports {
+				_, found := b.importSet[importSpec.Path.Value]
+				if !found {
+					hasNewDependency = true
+				}
+				b.importSet[importSpec.Path.Value] = true
+			}
+		}
+	}
+	return hasNewDependency
 }
